a5a3d8e9e1de8cedefe0e500c2c41fae53123014
Alikhan Boltekov <boltekovalihan@gmail.com>
2026-04-07 19:37:01 +0600

    firstversion

---
+++ Controllers/LCMController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LowestCommonMultiple.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LCMController : ControllerBase
+    {
+        [HttpGet]
+        public String LCM(int x, int y) {
+            if (x >= 0 && y >= 0) {
+                int i = 1;
+                while ((x * i) % y != 0) {
+                    i++;
+                }
+                return (x * i).ToString();
+            }
+            return "NaN";
+        }
+       
+    }
+}
